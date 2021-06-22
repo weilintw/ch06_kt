e787b2457ce40d44d84b4631e6088eba95476f5b
wl <kcwl558106@gmail.com>
2021-06-22 17:31:52 +0800

    c6-7聯合使用let和安全呼叫運算子

--- Tavern.kt
+++ Tavern.kt
@@ -1,7 +1,14 @@
-/*c6-6 使用安全呼叫運算子 ?.
+import java.util.*
+/*c6-7 聯合使用 let 和安全呼叫運算子
 */
-fun main(){
-    var beverage = readLine()?.capitalize()
+fun main (){
+    var beverage = readLine()?.let {
+        if (it.isNotBlank()){
+            it.replaceFirstChar { if ( it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString()}
+        } else {
+            "Buttered Ale"
+        }
+    }
     // beverage = null
     println(beverage)
 }
