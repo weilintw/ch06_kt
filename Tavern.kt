f00f3451c6246e09abe063c6720446b11c492959
wl <kcwl558106@gmail.com>
2021-06-22 17:49:31 +0800

    c6-13抛出IllegalStateException

--- Tavern.kt
+++ Tavern.kt
@@ -1,11 +1,15 @@
-/*c6-12 增加第3把劍，程式可能因未捕獲異常
+/*c6-13 抛出 IllegalStateException
 */
-fun main(){
-    var swordsJuqqling: Int? = null
+fun main (){
+    var swordsJuqqling: Int ? = null
     val isJigglingProficient = (1..3).shuffled().last() == 3
     if(isJigglingProficient){
         swordsJuqqling = 2
     }
+    proficiencyCheck(swordsJuqqling)
     swordsJuqqling = swordsJuqqling!!.plus(1)
-    println("You juggle $swordsJuqqling swords!")
+    println("You juggle $swordsJuqqling swords！")
+}
+fun proficiencyCheck(swordsJuqqling: Int?) {
+    swordsJuqqling ?: throw IllegalStateException("Player cannot juggle swords")
 }
