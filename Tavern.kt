23d5b6f466ee52c04f3213dbeec36af3977c6bda
wl <kcwl558106@gmail.com>
2021-06-22 18:00:08 +0800

    c6-15抛出自訂異常

--- Tavern.kt
+++ Tavern.kt
@@ -1,19 +1,17 @@
-/*c6-14 自訂異常
+/*c6-15 抛出自訂異常
 */
 fun main(){
-    var swordsJuqqling: Int? = null
+    var swordsJuqqling: Int ? = null
     val isJigglingProficient = (1..3).shuffled().last() == 3
-    if(isJigglingProficient){
+    if (isJigglingProficient){
         swordsJuqqling = 2
     }
     proficiencyCheck(swordsJuqqling)
     swordsJuqqling = swordsJuqqling!!.plus(1)
-    println("You juggle $swordsJuqqling swords！")
+    println("You juggle $swordsJuqqling swords！"")
 }
-fun proficiencyCheck(swordsJuqqling: Int?) {
-    swordsJuqqling ?: throw IllegalStateException("Player cannot juggle swords")
-
-    class UnskilledSwordJugglerException() {
-        IllegalStateException("Player cannot juggle swords")
-    }
+fun proficiencyCheck(swordsJuqqling : Int?) {
+    swordsJuqqling ?: throw UnskilledSwordJugglerException()
 }
+class UnskilledSwordJugglerException():
+        IllegalStateException("玩家不會耍劍......")
