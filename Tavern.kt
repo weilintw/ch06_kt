9ac2345ba9d3cf846754c491ea4b8f56a0704772
wl <kcwl558106@gmail.com>
2021-06-22 18:27:13 +0800

    p6-19require先決條件測試

--- Tavern.kt
+++ Tavern.kt
@@ -1,25 +1,26 @@
-/*c6-17 使用先決條例 checkNotNull
+/*p6-19 require 先決條件測試
 */
 import java.lang.IllegalStateException
 fun main(){
-    var swordsJuqqling: Int ? = null
+    var swordsJuggling: Int ? = null
     val isJigglingProficient = (1..3).shuffled().last() == 3
     /* isJigglingProficient 取隨機值1~3，最大為3 */
-    if(isJigglingProficient) swordsJuqqling = 2
+    if (isJigglingProficient) swordsJuggling = 2
     try{
-        proficiencyCheck(swordsJuqqling)
-        swordsJuqqling = swordsJuqqling!!.plus(1)
-    } catch (e:Exception){
+        proficiencyCheck(swordsJuggling)
+        swordsJuggling = swordsJuggling!!.plus(1)
+        } catch (e:Exception){
         print(e)
     }
-    println("You juggle $swordsJuqqling swords！")
+    if(swordsJuggling == null) swordsJuggling = 0
+    println("你要了 $swordsJuggling 把劍！")
+    if(swordsJuggling == 0) juggleSwords(0)
 }
-fun proficiencyCheck(swordsJuqqling: Int?) {
+fun proficiencyCheck(swordsJuggling: Int?){
     //swordsJuqqling ?: throw UnskilledSwordJugglerException()
-    checkNotNull(swordsJuqqling,{"玩家不會耍劍......"})
+    checkNotNull(swordsJuggling,{"玩家不會耍劍......"})
 }
-/*
-class UnskilledSwordJugglerException():
-    IllegalStateException("玩家不會耍劍......")
-*/
+fun juggleSwords(swordsJuggling: Int){
+    require(swordsJuggling >= 3,{"至少要耍3把劍才夠刺激"})
+}
 
