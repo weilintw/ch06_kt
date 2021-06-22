2332c9237265304940e558c2a0ce3c8ed151595f
wl <kcwl558106@gmail.com>
2021-06-22 17:24:39 +0800

    c6-5會發生編譯時明錯誤,因capitalize未考慮讀取值可能為空

--- Tavern.kt
+++ Tavern.kt
@@ -1,7 +1,7 @@
-/*c6-4 註解一行程式
+/*c6-5 會發生編譯時明錯誤,因capitalize未考慮讀取值可能為空
 */
 fun main(){
-    var beverage = readLine()
-    //beverage = null
+    var beverage = readLine()?.capitalize()
+    // beverage = null
     println(beverage)
 }
